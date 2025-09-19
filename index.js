24548425da84d0dab682854d0b7084718aec8a21
235gh <otoobongiwoh@gmail.com>
2025-09-19 16:48:38 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+console.log(saveEl);
+console.log(countEl);
+
+let count = 0;
+
+function increment() {
+    count += 1;
+    countEl.textContent = count;
+    console.log(count);
+
+}
+
+
+
+
+
+
+function save() {
+    let countStr = count + "  -  ";
+    saveEl.textContent += countStr;
+    countEl.textContent = 0;
+    count = 0;
+    console.log(count);
+}
+
+
+
+
